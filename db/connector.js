fdb88cb861434e53ac062db832852b8c12f7814d
classuser311 <classuser311@local.iful.edu.ua>
2026-03-30 10:25:31 +0300

    Better view

--- db/connector.js
+++ db/connector.js
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
-import { create } from 'hbs';
 dotenv.config();
 import pg from 'pg';
 const { Pool } = pg;
@@ -9,46 +8,5 @@ const pool = new Pool({
 });
 
 //Add table creation
-
-const createTableQueries = [];
-
-createTableQueries.push(`
-    CREATE TABLE IF NOT EXISTS heroes1 (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,              
-    primary_attribute TEXT,        
-    role TEXT,       
-    attack_type TEXT,           
-    difficulty INTEGER,                
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-   `);
-createTableQueries.push(`
- CREATE TABLE IF NOT EXISTS sloniki1 (
-    id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
-    age TEXT NOT NULL,
-    place_of_birth TEXT NOT NULL,           
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-   );
-      `);
-
-createTableQueries.push(`
- CREATE TABLE IF NOT EXISTS games_info (
-    id SERIAL PRIMARY KEY,
-    game_name TEXT NOT NULL,
-    game_mode TEXT NOT NULL,      
-    cost TEXT NOT NULL,   
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-   `);
-
-for (let i = 0; i < createTableQueries.length; i++) {
-    console.log(`create string`)
-    pool.query(createTableQueries[i]);
-}
-
-console.log("CONNECTED!!!!!✅ ")
 
 export default pool;
